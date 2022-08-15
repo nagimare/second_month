796f6bfb8cbbf128d3a56738530e0761b95b6b91
mirbek1 <109955612+mirbek1@users.noreply.github.com>
2022-08-15 14:49:16 +0600

    ok

---
+++ models.py
@@ -0,0 +1,28 @@
+from peewee import *
+
+db = PostgresqlDatabase(
+    'countries2',
+    host = 'localhost',
+    port = '5432',
+    user = 'cola',
+    password = '123'
+)
+db.connect()
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+        
+class Countries2(BaseModel):
+    name =  CharField(max_length=255, null = False)
+    official_name =  CharField(max_length=255, null = False)
+    capital = CharField(max_length=255, null = False)
+    region =  CharField(max_length=255, null = False)
+    subregion = CharField(max_length=255, null = False)
+    population = IntegerField(null = False)
+    continents = CharField(max_length=255, null = False)
+    timezones = CharField(max_length=255, null = False)
+    
+
+
+db.create_tables([Countries2]) 
